2f9aa1e5e7aa695f9687088010a33591651b46b4
eagle <eagle-eyed@email.com>
2016-11-13 23:12:44 +0200

    Add skip instruction

---
+++ com.dyalog.apldev.debug.ui/src/com/dyalog/apldev/debug/ui/actions/SkipCurLineCommandActionDelegate.java
@@ -0,0 +1,7 @@
+package com.dyalog.apldev.debug.ui.actions;
+
+import org.eclipse.debug.internal.ui.commands.actions.DebugCommandActionDelegate;
+
+public class SkipCurLineCommandActionDelegate extends DebugCommandActionDelegate {
+
+}
